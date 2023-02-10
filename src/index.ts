b1e15e388ff8fd5b72dc9f8eff6e1bbe27f34f80
questDI <ignatovich.dm@gmail.com>
2023-02-10 12:41:08 +0100

    basic graphql schema

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import { ApolloServer, } from 'apollo-server'
 import { typeDefs } from './schema'
 import { Query } from './resolvers'
+import { PrismaClient, Prisma } from '@prisma/client'
+
+export interface Context {
+    prisma: PrismaClient<Prisma.PrismaClientOptions, never, Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined>
+}
+
+const prisma = new PrismaClient()
 
 const server = new ApolloServer({
     typeDefs,
     resolvers: {
         Query
+    },
+    context: {
+        prisma
     }
 })
 
--- src/schema.ts
+++ src/schema.ts
@@ -4,4 +4,40 @@ export const typeDefs = gql`
     type Query {
         hello: String!
     }
+
+    type Mutation {
+        postCreate(title: String!, content: String!): PostPayload!
+    }
+
+    type Post {
+        id: ID!
+        tile: String!
+        content: String!
+        createdAt: String!
+        published: Boolean!
+        user: User!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        profile: Profile!
+        posts: [Post]!
+    }
+
+    type Profile {
+        id: ID!
+        bio: String
+        user: User!
+    }
+
+    type UserError {
+        message: String!
+    }
+
+    type PostPayload {
+        userErrors: [UserError!]!
+        post: Post
+    }
 `
